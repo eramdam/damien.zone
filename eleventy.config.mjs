d46cd4f58b834a16105380d1f906dead1fa47b36
Damien Erambert <damien@erambert.me>
2024-12-27 15:31:17 +0100

    working head include

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,8 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPassthroughCopy("assets/*.css");
   eleventyConfig.addLayoutAlias("base", "base.html");
   eleventyConfig.setLiquidOptions({
