040dd2a073712d1ce2af4974e04b53647853370b
Damien Erambert <damien@erambert.me>
2024-12-27 15:44:14 +0100

    moar includes

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,9 +1,15 @@
+import eleventyAutoCacheBuster from "eleventy-auto-cache-buster";
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyAutoCacheBuster, {
+    enableLogging: true,
+  });
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
   eleventyConfig.addPassthroughCopy("assets/*.css");
+  eleventyConfig.addPassthroughCopy("assets/*.js");
   eleventyConfig.addLayoutAlias("base", "base.html");
   eleventyConfig.setLiquidOptions({
     // https://liquidjs.com/tutorials/options.html#jsTruthy
