e242b6f568f99d8ba8d34ca0ccca5d9b2146d9e5
Damien Erambert <damien@erambert.me>
2024-12-27 15:30:38 +0100

    define liquid options

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -2,4 +2,11 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/*.css");
   eleventyConfig.addLayoutAlias("base", "base.html");
+  eleventyConfig.setLiquidOptions({
+    // https://liquidjs.com/tutorials/options.html#jsTruthy
+    jsTruthy: true,
+    // https://liquidjs.com/tutorials/options.html#Strict
+    strictVariables: true,
+    strictFilters: true,
+  });
 }
