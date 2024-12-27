e441ad4ebd865dee05c9a6a1276dc5117b21caa8
Damien Erambert <damien@erambert.me>
2024-12-27 15:44:14 +0100

    moar includes

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+(() => {
+  const urlsAllowList = ["damien.zone", "127.0.0.1:3000", "localhost:8080"];
+  if (urlsAllowList.every((url) => !window.location.href.includes(url))) {
+    return;
+  }
+  const mainPost = document.querySelector("body.post > main");
+  // If we're not on a post page, nothing to do.
+  if (!mainPost) {
+    return;
+  }
+
+  mainPost.insertAdjacentHTML(
+    "beforeend",
+    `<comentario-comments live-update="false" no-fonts="true"></comentario-comments>`,
+  );
+
+  const script = document.createElement("script");
+  script.setAttribute(
+    "src",
+    "https://comments.friendsofeggbug.org/comentario.js",
+  );
+  mainPost.insertAdjacentElement("beforeend", script);
+})();
