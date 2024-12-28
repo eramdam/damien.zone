f4883576989990e345bf4d11b20e2574170e640c
Damien Erambert <damien@erambert.me>
2024-12-28 23:25:18 +0100

    footnotes

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,20 +1,23 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { helpersPlugin } from "./11ty/helpers.mjs";
+import markdownItFootnote from "markdown-it-footnote";
 
-/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
-export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-  eleventyConfig.addPlugin(helpersPlugin);
+/** @param {import("@11ty/eleventy").UserConfig} config */
+export default function (config) {
+  config.addPlugin(syntaxHighlight);
+  config.addPlugin(EleventyHtmlBasePlugin);
+  config.addPlugin(helpersPlugin);
+
+  config.amendLibrary("md", (mdLib) => mdLib.use(markdownItFootnote));
 
   // Styles, scripts and media used by the general layout
-  eleventyConfig.addPassthroughCopy("src/assets");
+  config.addPassthroughCopy("src/assets");
   // Buttons, stuff people might want to grab/reference
-  eleventyConfig.addPassthroughCopy("src/public");
+  config.addPassthroughCopy("src/public");
   // Images used inside articles/pages
-  eleventyConfig.addPassthroughCopy("src/img");
-  eleventyConfig.setLiquidOptions({
+  config.addPassthroughCopy("src/img");
+  config.setLiquidOptions({
     // https://liquidjs.com/tutorials/options.html#jsTruthy
     jsTruthy: true,
     // https://liquidjs.com/tutorials/options.html#Strict
@@ -22,9 +25,9 @@ export default function (eleventyConfig) {
     strictFilters: true,
   });
 
-  eleventyConfig.setInputDirectory("src");
-  eleventyConfig.setOutputDirectory("_site");
-  eleventyConfig.setServerOptions({
+  config.setInputDirectory("src");
+  config.setOutputDirectory("_site");
+  config.setServerOptions({
     port: 3000,
   });
 }
