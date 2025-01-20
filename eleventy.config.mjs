0e36e170950e92512930283e58d03505ee573134
Damien Erambert <damien@erambert.me>
2025-01-19 20:56:22 -0800

    Update eleventy.config.mjs

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -14,6 +14,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(helpersPlugin);
   eleventyConfig.addPlugin(feedsPlugin);
+  eleventyConfig.setQuietMode(true);
   eleventyConfig.addPlugin(eleventyAutoCacheBuster, {
     globstring: "/{assets,img/blog,img/projects}/**/*",
     extensions: ["js", "png", "jpg", "jpeg", "gif", "mp4", "ico", "webp"],
