bc5844151881a13cdf4ffb076b22e50c628d1452
Damien Erambert <damien@erambert.me>
2025-05-11 20:13:49 -0700

    figure images

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,8 @@
-import remarkAttributes from "remark-attributes";
 import { defineConfig, passthroughImageService } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
+import { rehypeFigures } from "./src/core/customRemarkFigure";
+
 const isDev = import.meta.env.DEV;
 
 export default defineConfig({
@@ -16,16 +17,18 @@ export default defineConfig({
   site: isDev ? "http://localhost:4321" : "https://damien.zone",
   compressHTML: false,
   markdown: {
-    // @ts-expect-error
-    remarkPlugins: [remarkAttributes],
+    rehypePlugins: [rehypeFigures],
   },
-  integrations: [expressiveCode({
-    themes: ["github-dark", "github-light"],
-    styleOverrides: {
-      frames: {
-        frameBoxShadowCssValue: "none",
+  integrations: [
+    expressiveCode({
+      themes: ["github-dark", "github-light"],
+      styleOverrides: {
+        frames: {
+          frameBoxShadowCssValue: "none",
+        },
+        codeFontFamily: "var(--font-monospace)",
       },
-      codeFontFamily: "var(--font-monospace)",
-    },
-  }), mdx()],
+    }),
+    mdx(),
+  ],
 });
