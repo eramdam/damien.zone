6002c93a8ad627de89fe1033d224e460561924b6
Damien Erambert <damien@erambert.me>
2025-05-11 21:23:59 -0700

    custom directive for video

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
+import remarkDirectiveRehype from "remark-directive-rehype";
 import { defineConfig, passthroughImageService } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 import { rewriteWithFigures } from "./src/core/customRemarkFigure";
 import rehypeRewrite from "rehype-rewrite";
+import remarkDirective from "remark-directive";
+import { customVideoDirective } from "./src/core/customVideoDirective";
 
 const isDev = import.meta.env.DEV;
 
@@ -18,11 +21,14 @@ export default defineConfig({
   site: isDev ? "http://localhost:4321" : "https://damien.zone",
   compressHTML: false,
   markdown: {
+    remarkPlugins: [remarkDirective, customVideoDirective as any],
     rehypePlugins: [
       [
         rehypeRewrite,
         {
-          rewrite: rewriteWithFigures,
+          rewrite: (node, index, parent) => {
+            rewriteWithFigures(node, index, parent);
+          },
         },
       ],
     ],
---
+++ src/core/customVideoDirective.ts
@@ -0,0 +1,83 @@
+import type { Doctype, ElementContent, Root } from "hast";
+import { visit } from "unist-util-visit";
+import type { Directives } from "mdast-util-directive";
+import type { VFile } from "vfile";
+import fs from "node:fs";
+import path from "node:path";
+
+export function customVideoDirective() {
+  return function (tree: Root, file: VFile) {
+    visit(tree, function (node: Root | Doctype | ElementContent | Directives) {
+      if (
+        node.type === "containerDirective" ||
+        node.type === "leafDirective" ||
+        node.type === "textDirective"
+      ) {
+        if (node.name !== "video") {
+          return;
+        }
+
+        const { attributes } = node;
+        const { src } = attributes;
+
+        if (node.type === "textDirective") {
+          if (node.type === "textDirective") {
+            file.fail(
+              "Unexpected `:video` text directive, use two colons for a leaf directive",
+              node,
+            );
+          }
+        }
+
+        if (!src) {
+          file.fail("missing `src` on `video` directive", node);
+        }
+
+        const data = node.data || (node.data = {});
+        console.log(node);
+
+        const srcParsed = path.parse(src);
+        console.log(srcParsed);
+        const poster = path.format({
+          ...srcParsed,
+          base: `${srcParsed.name}-poster.webp`,
+          name: `${srcParsed.name}-poster`,
+          ext: ".webp",
+        });
+
+        const caption =
+          node.children?.[0].type === "text" && node.children[0].value;
+
+        data.hName = "figure";
+        data.hProperties = {
+          "data-type": "video",
+        };
+        data.hChildren = [
+          {
+            type: "element",
+            tagName: "video",
+            children: [],
+            properties: {
+              src,
+              poster,
+              playsinline: "",
+              controls: "",
+              preload: "none",
+            },
+          },
+          caption && {
+            type: "element",
+            tagName: "figcaption",
+            children: [
+              {
+                type: "text",
+                value: caption,
+              },
+            ],
+            properties: {},
+          },
+        ];
+      }
+    });
+  };
+}
