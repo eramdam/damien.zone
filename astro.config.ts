62dbce9c636af4eda44dab83068b9baeef53b9cf
Damien Erambert <damien@erambert.me>
2025-05-11 18:12:41 -0700

    codeblocks

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig, passthroughImageService } from "astro/config";
+import expressiveCode from "astro-expressive-code";
 const isDev = import.meta.env.DEV;
 
 export default defineConfig({
   image: {
     service: passthroughImageService(),
   },
+
   devToolbar: {
     enabled: false,
   },
+
   site: isDev ? "http://localhost:4321" : "https://damien.zone",
   compressHTML: false,
+  integrations: [
+    expressiveCode({
+      themes: ["github-dark", "github-light"],
+      styleOverrides: {
+        frames: {
+          frameBoxShadowCssValue: "none",
+        },
+        codeFontFamily: "var(--font-monospace)",
+      },
+    }),
+  ],
 });
