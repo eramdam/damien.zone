a894a28185d728b502761f689cdcd9367ef0e98e
Damien Erambert <damien@erambert.me>
2025-05-12 22:00:42 -0700

    add checking of broken links

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import expressiveCode from "astro-expressive-code";
 import { defineConfig, passthroughImageService } from "astro/config";
 import rehypeRewrite from "rehype-rewrite";
 import { rewriteWithFigures } from "./src/core/customRemarkFigure";
+import astroBrokenLinksChecker from "astro-broken-link-checker";
 
 const isDev = import.meta.env.DEV;
 
@@ -44,5 +45,9 @@ export default defineConfig({
       },
     }),
     mdx(),
+    astroBrokenLinksChecker({
+      logFilePath: "broken-links.log", // Optional: specify the log file path
+      checkExternalLinks: false, // Optional: check external links (currently, caching to disk is not supported, and it is slow )
+    }),
   ],
 });
