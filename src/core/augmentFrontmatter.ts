9e93e2f0e16ca30f2955810e74b9050b20758cef
Damien Erambert <damien@erambert.me>
2025-05-14 10:05:15 -0700

    git modified date

--- src/core/augmentFrontmatter.ts
+++ src/core/augmentFrontmatter.ts
@@ -3,6 +3,8 @@ import type { Root } from "hast";
 import fs from "node:fs";
 import type { VFile } from "vfile";
 
+import { getLastUpdatedTimestamp } from "./gitDate";
+
 export function augmentFrontmatterFields() {
   // All remark and rehype plugins return a separate function
   return function (_tree: Root, file: VFile) {
@@ -10,7 +12,7 @@ export function augmentFrontmatterFields() {
       file.data.astro.frontmatter.image = "/open_graph.webp";
     }
 
-    // Convert `now` in the date to current date, like 🐻 blog did.
+    // Convert `now` in the date to current date, like bearblog did.
     if (file.data.astro.frontmatter.date === "now") {
       const newDate = new Date();
       file.data.astro.frontmatter.date = newDate.toISOString();
@@ -19,6 +21,16 @@ export function augmentFrontmatterFields() {
         file.data.astro.frontmatter,
       );
       fs.writeFileSync(file.history[0], fileString);
+    }
+    const filePath = file.history[0];
+    const cutoff = new Date(`2025-05-14T17:04:02.168Z`);
+
+    // Add a `updated` field that uses the Git modified date
+    if (!file.data.astro.frontmatter.updated && filePath.includes("src/blog")) {
+      const updated = getLastUpdatedTimestamp(filePath);
+      if (updated > cutoff) {
+        file.data.astro.frontmatter.updated = updated;
+      }
     }
   };
 }
