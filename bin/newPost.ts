4eb522887d8eb313f65a6fc02671d7c16bfc20de
Damien Erambert <damien@erambert.me>
2026-07-01 09:51:25 -0700

    use date-fns for prefix in newpost.ts

--- bin/newPost.ts
+++ bin/newPost.ts
@@ -2,14 +2,12 @@ import matter from "gray-matter";
 import { spawnSync } from "node:child_process";
 import fs from "node:fs";
 import { generateSlug } from "random-word-slugs";
+import { format } from "date-fns";
 
 const defaultSlug = generateSlug(4);
 const date = new Date();
 const year = date.getFullYear();
-const prefix =
-  (date.getMonth() + 1).toString().padStart(2, "0") +
-  "-" +
-  date.getDate().toString().padStart(2, "0").toString().padStart(2, "0");
+const prefix = format(date, "MM-dd");
 const filename = `${prefix}-${defaultSlug}.md`;
 
 const fileContent = matter.stringify("", {
