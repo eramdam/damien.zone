9c0bd3a363267da24d9affae5a977672312c0502
Damien Erambert <damien@erambert.me>
2026-07-03 22:09:30 -0700

    🔧

--- admin/astro.config.ts
+++ admin/astro.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import node from "@astrojs/node";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
@@ -6,6 +7,9 @@ import { fileURLToPath } from "node:url";
 const adminDir = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   root: adminDir,
   adapter: node({
     mode: "standalone",
@@ -13,4 +17,11 @@ export default defineConfig({
   cacheDir: path.join(adminDir, ".astro"),
   srcDir: path.join(adminDir, "src"),
   publicDir: path.join(adminDir, "public"),
+  vite: {
+    plugins: [
+      nodePolyfills({
+        include: ["buffer"],
+      }),
+    ],
+  },
 });
