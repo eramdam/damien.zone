33b0d196e3a9a202c0630949a3453c4f272a978e
Damien Erambert <damien@erambert.me>
2026-07-03 22:38:27 -0700

    fix theme build

--- admin/astro.config.ts
+++ admin/astro.config.ts
@@ -3,8 +3,23 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import node from "@astrojs/node";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import fs from "node:fs";
 
 const adminDir = path.dirname(fileURLToPath(import.meta.url));
+
+const copyThemes = () => {
+  const files = fs
+    .globSync("../node_modules/monaco-themes/themes/*.json", {
+      cwd: adminDir,
+    })
+    .map((f) => path.resolve(adminDir, f));
+
+  for (const file of files) {
+    fs.cpSync(file, path.resolve(adminDir, `./themes/${path.basename(file)}`));
+  }
+};
+
+copyThemes();
 
 export default defineConfig({
   image: {
