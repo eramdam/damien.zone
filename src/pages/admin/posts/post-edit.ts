ada385c2b53f8e14242426984bcaf93e3c8a0f6c
Damien Erambert <damien@erambert.me>
2026-07-03 19:37:47 -0700

    attrs editor

--- src/pages/admin/posts/post-edit.ts
+++ src/pages/admin/posts/post-edit.ts
@@ -1,3 +1,4 @@
+import matter from "gray-matter";
 import { actions } from "astro:actions";
 import { navigate } from "astro:transitions/client";
 import * as monaco from "monaco-editor";
@@ -17,8 +18,6 @@ declare global {
 if (form) {
   const initialPostData = window.initData;
 
-  const tm = monaco.editor.createModel(initialPostData.body || "", "markdown");
-
   monaco.editor.defineTheme("md-dark", {
     ...window.darkTheme,
     colors: {
@@ -28,44 +27,106 @@ if (form) {
   monaco.editor.defineTheme("md-light", window.lightTheme);
 
   const mdDark = window.matchMedia("(prefers-color-scheme:dark)");
+  const commonEditorOptions = {
+    theme: mdDark.matches ? "md-dark" : "md-light",
+    // Fonts
+    fontSize: 16,
+    wordWrap: "on",
+    lineNumbers: "off",
+    glyphMargin: false,
+    minimap: { enabled: false },
+    matchBrackets: "never",
+    // Hide overview rules (right lanes)
+    hideCursorInOverviewRuler: true,
+    overviewRulerLanes: 0,
+    scrollBeyondLastLine: false,
+    automaticLayout: false,
+    scrollbar: {
+      vertical: "auto",
+      verticalScrollbarSize: 8,
+      useShadows: false,
+    },
+    renderLineHighlight: "none",
+  } satisfies monaco.editor.IStandaloneEditorConstructionOptions;
 
+  const bodyModel = monaco.editor.createModel(
+    initialPostData.body || "",
+    "markdown",
+  );
   const bodyEditor = monaco.editor.create(
     document.querySelector("#post-edit-area")!,
     {
-      model: tm,
-      // Language
-      language: "markdown",
-      theme: mdDark.matches ? "md-dark" : "md-light",
+      model: bodyModel,
       fontFamily:
         "Maple Mono, Hack, 'Consolas', Monaco, 'Courier New', monospace",
-      // Fonts
-      fontSize: 16,
-      wordWrap: "on",
-      lineNumbers: "off",
-      glyphMargin: false,
-      minimap: { enabled: false },
-      matchBrackets: "never",
-      // Hide overview rules (right lanes)
-      hideCursorInOverviewRuler: true,
-      overviewRulerLanes: 0,
-      scrollBeyondLastLine: false,
-      automaticLayout: true,
-      scrollbar: {
-        vertical: "auto",
-        verticalScrollbarSize: 8,
-        useShadows: false,
+      // Language
+      language: "markdown",
+      ...commonEditorOptions,
+      padding: {
+        top: 20,
       },
     },
   );
+
+  const attrsRaw = matter
+    .stringify("", initialPostData.data)
+    .replaceAll("---", "")
+    .trim();
+  const attrsModel = monaco.editor.createModel(attrsRaw, "yaml");
+
+  const attrsEditor = monaco.editor.create(
+    document.querySelector("#attrs-edit-area")!,
+    {
+      model: attrsModel,
+      fontFamily: "Verdana, sans-serif",
+      // Language
+      language: "yaml",
+      ...commonEditorOptions,
+      automaticLayout: false,
+      padding: {
+        top: 10,
+        bottom: 10,
+      },
+    },
+  );
+
+  const updateAttrsHeight = (e: { contentHeightChanged: boolean }) => {
+    if (!e.contentHeightChanged) {
+      return;
+    }
+    attrsEditor.layout({
+      height: attrsEditor.getContentHeight(),
+      width: form.clientWidth,
+    });
+    const wrapper = document.querySelector<HTMLDivElement>(
+      "#attrs-edit-wrapper",
+    );
+    if (wrapper) {
+      wrapper.style.height = attrsEditor.getContentHeight() + "px";
+    }
+  };
+
+  attrsEditor.onDidContentSizeChange(updateAttrsHeight);
+  updateAttrsHeight({ contentHeightChanged: true });
 
   mdDark.addEventListener("change", (e) => {
     bodyEditor.updateOptions({
       theme: e.matches ? "md-dark" : "md-light",
       automaticLayout: true,
     });
+    attrsEditor.updateOptions({
+      theme: e.matches ? "md-dark" : "md-light",
+      automaticLayout: true,
+    });
   });
 
-  tm.updateOptions({
+  bodyModel.updateOptions({
+    bracketColorizationOptions: {
+      enabled: false,
+      independentColorPoolPerBracketType: false,
+    },
+  });
+  attrsModel.updateOptions({
     bracketColorizationOptions: {
       enabled: false,
       independentColorPoolPerBracketType: false,
