20e80e3820470bfd276070e75b5deab71b11fd8f
Damien Erambert <damien@erambert.me>
2026-07-03 18:56:25 -0700

    theme stuff + attributes

--- src/helpers/postsHelpers.ts
+++ src/helpers/postsHelpers.ts
@@ -1,4 +1,5 @@
 import { getCollection } from "astro:content";
+import { uniq } from "es-toolkit";
 
 export async function getBlogPosts() {
   return (await getCollection("blog")).filter((b) => !b.data.isDraft);
@@ -6,4 +7,8 @@ export async function getBlogPosts() {
 
 export async function getDrafts() {
   return (await getCollection("blog")).filter((b) => b.data.isDraft);
+}
+
+export async function getAllTags() {
+  return uniq((await getBlogPosts()).flatMap((p) => p.data.tags)).toSorted();
 }
--- src/pages/admin/posts/post-edit.ts
+++ src/pages/admin/posts/post-edit.ts
@@ -1,6 +1,7 @@
 import { actions } from "astro:actions";
 import { navigate } from "astro:transitions/client";
 import * as monaco from "monaco-editor";
+import type { MonacoTheme } from "monaco-themes";
 import type { getBlogPosts } from "../../../helpers/postsHelpers";
 
 const form = document.querySelector("form");
@@ -8,6 +9,8 @@ const form = document.querySelector("form");
 declare global {
   interface Window {
     initData: Awaited<ReturnType<typeof getBlogPosts>>[number];
+    darkTheme: MonacoTheme;
+    lightTheme: MonacoTheme;
   }
 }
 
@@ -15,24 +18,51 @@ if (form) {
   const initialPostData = window.initData;
 
   const tm = monaco.editor.createModel(initialPostData.body || "", "markdown");
-  monaco.editor.create(document.querySelector("#post-edit-area")!, {
-    model: tm,
-    // Language
-    language: "markdown",
-    theme: "vs-dark",
-    fontFamily:
-      "Maple Mono, Hack, 'Consolas', Monaco, 'Courier New', monospace",
-    // Fonts
-    fontSize: 16,
-    wordWrap: "on",
-    lineNumbers: "off",
-    minimap: { enabled: false },
-    matchBrackets: "never",
-    // Scrolling
-    scrollBeyondLastLine: false,
-    smoothScrolling: true,
-    // Performance
-    automaticLayout: true,
+
+  monaco.editor.defineTheme("md-dark", {
+    ...window.darkTheme,
+    colors: {
+      "editor.background": "#ffffff04",
+    },
+  });
+  monaco.editor.defineTheme("md-light", window.lightTheme);
+
+  const mdDark = window.matchMedia("(prefers-color-scheme:dark)");
+
+  const bodyEditor = monaco.editor.create(
+    document.querySelector("#post-edit-area")!,
+    {
+      model: tm,
+      // Language
+      language: "markdown",
+      theme: mdDark.matches ? "md-dark" : "md-light",
+      fontFamily:
+        "Maple Mono, Hack, 'Consolas', Monaco, 'Courier New', monospace",
+      // Fonts
+      fontSize: 16,
+      wordWrap: "on",
+      lineNumbers: "off",
+      glyphMargin: false,
+      minimap: { enabled: false },
+      matchBrackets: "never",
+      // Hide overview rules (right lanes)
+      hideCursorInOverviewRuler: true,
+      overviewRulerLanes: 0,
+      scrollBeyondLastLine: false,
+      automaticLayout: true,
+      scrollbar: {
+        vertical: "auto",
+        verticalScrollbarSize: 8,
+        useShadows: false,
+      },
+    },
+  );
+
+  mdDark.addEventListener("change", (e) => {
+    bodyEditor.updateOptions({
+      theme: e.matches ? "md-dark" : "md-light",
+      automaticLayout: true,
+    });
   });
 
   tm.updateOptions({
@@ -45,6 +75,7 @@ if (form) {
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
     const fd = new FormData(form);
+    fd.set("body", bodyEditor.getValue());
     console.log(await actions.updatePost(fd));
   });
 
