95694f25a7bcbc5549789cf6e8190ff8a505cab3
Damien Erambert <damien@erambert.me>
2026-07-30 18:42:25 -0700

    consistent page id for comentario

--- src/assets/comments.mjs
+++ src/assets/comments.mjs
@@ -3,10 +3,16 @@ if (urlsAllowList.some((url) => window.location.href.includes(url))) {
   const mainPost = document.querySelector("body.post > main");
   // If we're not on a post page, nothing to do.
   if (mainPost) {
-    mainPost.insertAdjacentHTML(
-      "beforeend",
-      `<comentario-comments live-update="false" no-fonts="true"></comentario-comments>`,
+    const { pathname } = window.location;
+    const commentsElement = document.createElement("comentario-comments");
+    commentsElement.setAttribute("live-updates", "false");
+    commentsElement.setAttribute("no-fonts", "true");
+    commentsElement.setAttribute(
+      "page-id",
+      pathname.endsWith("/") ? pathname : `${pathname}/`,
     );
+
+    mainPost.insertAdjacentElement("beforeend", commentsElement);
 
     const script = document.createElement("script");
     script.setAttribute(
