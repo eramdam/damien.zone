dc41db25ab2c71a33867d1a7d313e3189e8ed331
Damien Erambert <damien@erambert.me>
2026-08-15 07:23:59 -0700

    grr

--- blog/astro.config.ts
+++ blog/astro.config.ts
@@ -15,7 +15,7 @@ console.log("import.meta.env", import.meta.env);
 console.log("process.env", process.env);
 const isDev = import.meta.env.DEV;
 const isCI =
-  import.meta.env.CI === "true" || import.meta.env.COOLIFY_URL !== undefined;
+  import.meta.env.CI === "true" || process.env.COOLIFY_URL !== undefined;
 const assetsPrefix = isCI ? "https://cdn.damien.zone" : undefined;
 
 export default defineConfig({
