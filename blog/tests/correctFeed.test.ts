5efd6a1bcaf8b111f88d3f285be334565e4f6c0e
Damien Erambert <damien@erambert.me>
2026-08-15 07:57:34 -0700

    add test for assetsPrefix

--- blog/tests/correctFeed.test.ts
+++ blog/tests/correctFeed.test.ts
@@ -2,6 +2,7 @@ import { XMLParser } from "fast-xml-parser";
 import assert from "node:assert";
 import fs from "node:fs";
 import { describe, it } from "node:test";
+import * as cheerio from "cheerio";
 const parser = new XMLParser();
 
 describe("building for production", () => {
@@ -12,6 +13,18 @@ describe("building for production", () => {
     assert(
       new URL(res.feed.id || "").host === "damien.zone",
       "RSS has damien.zone",
+    );
+  });
+
+  it("has right assetsPrefix", () => {
+    assert(fs.existsSync("./dist/index.html"), "index.html exists");
+
+    const htmlRaw = fs.readFileSync("./dist/index.html");
+    const $ = cheerio.load(htmlRaw);
+    const prefix = $("meta[name=assetsPrefix]").attr("content");
+    assert(
+      prefix === "https://cdn.damien.zone",
+      "prefix is https://cdn.damien.zone",
     );
   });
 });
